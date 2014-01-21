1afa36119b0ae3cbf5b62c4aab8e856b3ae894d5
Mohannad Abwah <mohannad.abwah@xtremelabs.com>
2014-01-20 23:29:41 -0500

    - Overrode onCreate - Created TextView - Set text on TextView - Attached textView to Activity using setContentView
    THIS MANIPULATES UI IN CODE. IT'S NOT RECOMMENDED, JUST AN EXAMPLE!

--- src/ca/uoftdev/helloworld/MainActivity.java
+++ src/ca/uoftdev/helloworld/MainActivity.java
@@ -1,7 +1,17 @@
 package ca.uoftdev.helloworld;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		TextView textView = new TextView(this);
+		textView.setText("Hello World!");
+		setContentView(textView);
+	}
 
 }
