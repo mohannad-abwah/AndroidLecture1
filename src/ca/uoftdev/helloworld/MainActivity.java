7725f95b11b2359b254a6011aed8c76dbfe0221f
Mohannad Abwah <mohannad.abwah@xtremelabs.com>
2014-01-20 23:31:45 -0500

    - Removed old code-based UI - Implemented exact same functionality using interactive XML

--- src/ca/uoftdev/helloworld/MainActivity.java
+++ src/ca/uoftdev/helloworld/MainActivity.java
@@ -2,16 +2,13 @@ package ca.uoftdev.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		TextView textView = new TextView(this);
-		textView.setText("Hello World!");
-		setContentView(textView);
+		setContentView(R.layout.activity_main);
 	}
 
 }
